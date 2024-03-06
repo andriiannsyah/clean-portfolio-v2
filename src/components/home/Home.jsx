a35edc63a4c270dd8da8a5aa87be9641fa63dce8
andriansyah <anandri394@gmail.com>
2024-03-07 00:56:19 +0700

    section header dan home

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,21 @@
+import Social from "./Social";
+import "./home.css";
+import Data from "./Data";
+import ScrollDown from "./ScrollDown";
+
+function Home() {
+  return (
+    <section className="home section" id="home">
+      <div className="home__container container grid">
+        <div className="home__content grid">
+          <Social />
+          <div className="home__image"></div>
+          <Data />
+        </div>
+        <ScrollDown />
+      </div>
+    </section>
+  );
+}
+
+export default Home;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,17 @@
+function Social() {
+  return (
+    <section className="home__social">
+      <a href="https://www.instagram.com/andriiannsyah/" className="home__social-icon" target="_blank">
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a href="" className="home__social-icon" target="_blank">
+        <i className="uil uil-github-alt"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/andriansyah-236b8521a/" className="home__social-icon" target="_blank">
+        <i className="uil uil-linkedin-alt"></i>
+      </a>
+    </section>
+  );
+}
+
+export default Social;
